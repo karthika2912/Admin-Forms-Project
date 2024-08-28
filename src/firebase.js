aa0826b27c112265173ed5ebacc252b3c594a459
Karthika <pappulakarthika123@gmail.com>
2024-08-28 15:01:16 +0530

    [admin-form] - minor update

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAON_k6-hg5MhrYCLW-7Ka-M1bo8b2XjEc",
+  apiKey: process.env.apiKey,
   authDomain: "user-feedback-338be.firebaseapp.com",
   projectId: "user-feedback-338be",
   storageBucket: "user-feedback-338be.appspot.com",
@@ -12,10 +12,7 @@ const firebaseConfig = {
   appId: "1:1038200858899:web:e869c3d9a8b72aca0852e6",
   measurementId: "G-CC1CQXBSFB"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
